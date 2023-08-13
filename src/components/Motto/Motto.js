b7cdd8c6394de6752b088decbf16e5ec4bd33b04
codekemi <130604147+codekemi@users.noreply.github.com>
2023-08-13 00:19:12 -0400

    Just some fine tuning

--- src/components/Motto/Motto.js
+++ src/components/Motto/Motto.js
@@ -1,11 +1,12 @@
 import styles from "./Motto.module.css";
 
-function Motto ()
-{
+function Motto() {
   return (
     <div className={styles["motto"]}>
-      <span className={styles["motto__text"]}>The solar system: the new home.</span>
-      <span className={styles["motto__emojis"]}>🌎🚀🧑‍🚀🪐</span>
+      <span className={styles["motto__text"]}>
+        The Solar System: Our New Home.
+      </span>
+      <span className={styles["motto__emojis"]}>🌎🚀🧑‍🚀🪐🛸</span>
     </div>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import styles from "./Home.module.css";
 
-function Home ()
-{
+function Home() {
   return (
     <>
       <section className={styles["section__container"]}>
@@ -10,28 +9,68 @@ function Home ()
 
       <section className={styles["section__container"]}>
         <h2>🌌 Journey into the Future</h2>
-        <p>In a world where the impossible has become reality, where the stars are no longer out of reach, welcome to the future of humanity's survival and exploration. Witness the evolution of technology as it transforms barren planets into thriving havens, all made possible by the wonders of innovation and human determination.</p>
+        <p>
+          In a world where the impossible has become reality, where the stars
+          are no longer out of reach, welcome to the future of humanity's
+          survival and exploration. Witness the evolution of technology as it
+          transforms barren planets into thriving havens, all made possible by
+          the wonders of innovation and human determination.
+        </p>
       </section>
 
       <section className={styles["section__container"]}>
         <h2>🌍 From Neglect to Innovation</h2>
-        <p>Once the cradle of civilization, Earth now stands as a solemn reminder of the consequences of neglect and environmental decline. But fear not, for the ingenuity of mankind has soared to new heights. With our relentless pursuit of advancement, we have not only healed our scars but extended our reach across the cosmos.</p>
+        <p>
+          Once the cradle of civilization, Earth now stands as a solemn reminder
+          of the consequences of neglect and environmental decline. But fear
+          not, for the ingenuity of mankind has soared to new heights. With our
+          relentless pursuit of advancement, we have not only healed our scars
+          but extended our reach across the cosmos.
+        </p>
       </section>
 
       <section className={styles["section__container"]}>
         <h2>🚀 Enter Space Travel: Where Dreams Take Flight</h2>
-        <p>Embark on an extraordinary journey with our groundbreaking web application, aptly named "Space Travel." As a commander engineer, the fate of humanity's exodus rests in your capable hands. Prepare to face the ultimate challenge: evacuating humankind from their birthplace and guiding them towards a future among the stars.</p>
+        <p>
+          Embark on an extraordinary journey with our groundbreaking web
+          application, aptly named "Space Travel." As a commander engineer, the
+          fate of humanity's exodus rests in your capable hands. Prepare to face
+          the ultimate challenge: evacuating humankind from their birthplace and
+          guiding them towards a future among the stars.
+        </p>
       </section>
 
       <section className={styles["section__container"]}>
         <h2>🔧 Engineer, Explorer, Leader</h2>
-        <p>Space Travel empowers you to engineer, design, and even dismantle spacecraft. Craft vessels that defy the boundaries of imagination, envisioning a future where life flourishes beyond the stars. But remember, your role extends beyond construction – you are a leader, an explorer, a commander steering humanity's destiny.</p>
+        <p>
+          Space Travel empowers you to engineer, design, and even dismantle
+          spacecraft. Craft vessels that defy the boundaries of imagination,
+          envisioning a future where life flourishes beyond the stars. But
+          remember, your role extends beyond construction – you are a leader, an
+          explorer, a commander steering humanity's destiny.
+        </p>
       </section>
 
       <section className={styles["section__container"]}>
         <h2>🌠 A Universe of Possibilities Awaits</h2>
-        <p>Immerse yourself in the thrill of exploration as you chart interplanetary courses within our solar system. Seamlessly navigate your fleet of spacecraft, hurtling through the cosmic void from one celestial body to another. The universe becomes your playground, and every planet a potential new home.</p>
+        <p>
+          Immerse yourself in the thrill of exploration as you chart
+          interplanetary courses within our solar system. Seamlessly navigate
+          your fleet of spacecraft, hurtling through the cosmic void from one
+          celestial body to another. The universe becomes your playground, and
+          every planet a potential new home.
+        </p>
+      </section>
 
+      <section className={styles["section__container"]}>
+        <h2>🛸Amidst the Cosmos' Canvas, Infinite Adventures Beckon</h2>
+        <p>
+          Embark on an extraterrestrial escapade as you pilot your cosmic crew
+          through the galaxy's tapestry. Glide between star-studded pit stops,
+          hopping from one celestial hotspot to another. Unveil the cosmos'
+          hidden treasures and turn distant orbs into your personal playgrounds.
+          Your spacefaring saga awaits – adventure beyond the stars!
+        </p>
       </section>
     </>
   );
